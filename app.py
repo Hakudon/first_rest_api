cccb7c67567d5297b7b70a2c89c0445af530cb26
Bijaya Shrestha <shrestha.bj2056@gmail.com>
2023-05-18 10:07:47 +0545

    Itme.py updated

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_smorest import Api
+from flask_jwt_extended import JWTManager
 
 from db import db
 
@@ -25,6 +26,8 @@ def create_app(db_url=None):
     app.config["PROPAGATE_EXCEPTIONS"] = True
     db.init_app(app)
     api = Api(app)
+    
+    
 
     with app.app_context():
         db.create_all()
